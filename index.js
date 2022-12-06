2d7e3666a44f8caa44301ba62d5da6be4ea7caa6
devsecopscloset <106809417+devsecopscloset@users.noreply.github.com>
2022-12-06 18:55:20 +0530

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello! Closet Server demosetup! namespaced Pipelines test \n');
+  res.send('Hello! Closet Server demosetup! namespaced Pipelines test again\n');
 
 });
 
